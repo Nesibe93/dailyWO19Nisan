df6698b50d2069902cae8c17727c33be9709004e
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-04-19 14:06:05 +0300

    Add project files.

---
+++ dailyWO19Nisan/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace dailyWO19Nisan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.dgwListe = new System.Windows.Forms.DataGridView();
+            this.chckbSec = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.Font = new System.Drawing.Font("Cascadia Code SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Location = new System.Drawing.Point(341, 70);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(194, 30);
+            this.cmbCountry.TabIndex = 0;
+            this.cmbCountry.SelectedIndexChanged += new System.EventHandler(this.cmbCountry_SelectedIndexChanged);
+            // 
+            // dgwListe
+            // 
+            this.dgwListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwListe.Location = new System.Drawing.Point(126, 145);
+            this.dgwListe.Name = "dgwListe";
+            this.dgwListe.RowHeadersWidth = 51;
+            this.dgwListe.RowTemplate.Height = 24;
+            this.dgwListe.Size = new System.Drawing.Size(842, 430);
+            this.dgwListe.TabIndex = 2;
+            // 
+            // chckbSec
+            // 
+            this.chckbSec.AutoSize = true;
+            this.chckbSec.Font = new System.Drawing.Font("Cascadia Code SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chckbSec.Location = new System.Drawing.Point(604, 72);
+            this.chckbSec.Name = "chckbSec";
+            this.chckbSec.Size = new System.Drawing.Size(152, 26);
+            this.chckbSec.TabIndex = 3;
+            this.chckbSec.Text = "Tüm Kayıtlar";
+            this.chckbSec.UseVisualStyleBackColor = true;
+            this.chckbSec.CheckedChanged += new System.EventHandler(this.chckbSec_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 621);
+            this.Controls.Add(this.chckbSec);
+            this.Controls.Add(this.dgwListe);
+            this.Controls.Add(this.cmbCountry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwListe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.DataGridView dgwListe;
+        private System.Windows.Forms.CheckBox chckbSec;
+    }
+}
+
---
+++ dailyWO19Nisan/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dailyWO19Nisan
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UlkeLoad();
+
+            cmbCountry.Items.Insert(0, "~Seçiniz~");
+            cmbCountry.SelectedIndex = 0;
+        }
+
+        private void UlkeLoad()
+        {
+            // Bağlantıyı aç
+            using (connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sorgu = "SELECT DISTINCT Country FROM Customers ORDER BY Country";
+
+                using (SqlCommand command = new SqlCommand(sorgu, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cmbCountry.Items.Add(reader["Country"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DataLoad()
+        {
+            using (connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sorgu = "SELECT CustomerID, CompanyName, ContactName, City, Country FROM Customers";
+
+                if (chckbSec.Checked)
+                {
+                    sorgu += " ORDER BY Country, City";
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(cmbCountry.Text))
+                    {
+                        sorgu += " WHERE Country=@Country ORDER BY City";
+                    }
+                }
+
+                using (SqlCommand command = new SqlCommand(sorgu, connection))
+                {
+                    if (!string.IsNullOrEmpty(cmbCountry.Text))
+                    {
+                        command.Parameters.AddWithValue("@Country", cmbCountry.Text);
+                    }
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        dgwListe.DataSource = table;
+                    }
+                }
+            }
+        }
+
+        private void chckbSec_CheckedChanged(object sender, EventArgs e)
+        {
+            DataLoad();
+            
+        }
+
+        private void cmbCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataLoad();
+        }
+    }
+}
